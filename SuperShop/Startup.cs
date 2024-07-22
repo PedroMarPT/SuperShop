e6c1fed9c5d07737ed681b0346bebd87c54bcd8d
PedroMarPT <mentesserias@gmail.com>
2024-07-22 18:36:23 +0100

    Repository

--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -32,7 +32,8 @@ namespace SuperShop
             });
 
             services.AddTransient<SeedDb>();
-       
+            services.AddScoped<IRepository,Repository>();
+
             services.AddControllersWithViews();
         }
 
